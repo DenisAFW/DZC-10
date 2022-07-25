dcd93d2432b4d8355dcd2d212f24966a4f75121a
Choppa <denis.afinogenow@yandex.ru>
2022-07-25 19:54:24 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив строк. Напишите программу,которая считает кол-во слов в массиве, начинающихся на гласную букву.
+
+int Current(string[] words, char[] vowels)
+{
+    int a = 0;
+
+    for (int i = 0; i < words.Length; i++)
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (words[i][0] == vowels[j])
+            {
+                a++;
+            }
+        }
+    return a;
+}
+
+char[] vowels = { 'a', 'e', 'i', 'u', 'o', 'y' };
+string[] Words = { "ywe", "wer", "ert", "rty", "ayu" };
+
+Console.WriteLine(Current(Words, vowels));
+
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+
+string[] Plus(string[] array)
+{
+    string[] sum = new string[array.Length / 2];
+
+    int j = 0;
+    for(int i = 0; i < sum.Length; i++)
+    {
+        sum[i] = array[j] + array[j+1];
+        j+=2;
+    }
+    return sum;
+}
+
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ,");
+    }
+    Console.WriteLine();
+}
+
+string[] massive = { "qwe", "wer", "ert", "rty", "tyu", "yui" };
+
+Print(Plus(massive));
